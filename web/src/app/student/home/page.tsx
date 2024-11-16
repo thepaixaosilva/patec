c0a87d7087cbac174af4ddd9f5d1de3dda5258ed
Sammy <samuelazevedo931@gmail.com>
2024-11-16 15:18:24 -0300

    Update some features

--- web/src/app/student/home/page.tsx
+++ web/src/app/student/home/page.tsx
@@ -6,7 +6,6 @@ import { Text } from '@chakra-ui/react'
 import { Image } from '@chakra-ui/react'
 import { List } from '@chakra-ui/react'
 import { Blockquote } from '@/components/ui/blockquote'
-
 export const metadata = {
   title: 'Home do Estudante',
   description: 'Bem-vindo à página inicial do estudante.',
@@ -21,20 +20,24 @@ export default function Home() {
         </Text>
       </div>
       <div className="flex flex-col items-center justify-center">
-        <List.Root>
-          <List.Item>AO CLICAR EM "INICIAR PROVA", VOCÊ SERÁ DIRECIONADO A UM GABARITO. PREENCHA COM CUIDADO ANTES DE CLICAR EM ENVIAR!</List.Item>
-          <List.Item>Add informação...</List.Item>
-          <List.Item>Add informação...</List.Item>
-          <List.Item>BOA PROVA!</List.Item>
-        </List.Root>
+        <Blockquote showDash cite="BOA PROVA">
+          <List.Root>
+            <List.Item>Ao clicar em "Iniciar Prova", você será redirecionado a um gabarito.</List.Item>
+            <List.Item>Cada disciplina possui uma prova com 5 questões de múltipla escolha.</List.Item>
+            <List.Item>Cada questão possui 5 alternativas, sendo apenas uma a correta.</List.Item>
+            <List.Item>Durante a realização da prova, o aluno selecionará uma, e somente uma alternativa por questão.</List.Item>
+            <List.Item>Cada questão correta equivale a 2 pontos, totalizando uma nota máxima de 10 pontos por disciplina.</List.Item>
+            <List.Item>Preencha com CUIDADO antes de clicar em enviar</List.Item>
+          </List.Root>
+        </Blockquote>
+
         <Image
-        src="https://img.freepik.com/premium-vector/cute-girl-children-student-uniforms-cat-study-sleepy-kawaii-cartoon-illustration_493693-111.jpg"
-        alt="Imagem 1"
-        width={300}
-        height={200}
-        className="rounded-lg border-4 border-white"
-      />
-       
+          src="https://img.freepik.com/premium-vector/cute-girl-children-student-uniforms-cat-study-sleepy-kawaii-cartoon-illustration_493693-111.jpg"
+          alt="Imagem 1"
+          width={300}
+          height={200}
+          className="rounded-lg border-4 border-white"
+        />
         <div />
 
         <div className="flex items-center justify-center">

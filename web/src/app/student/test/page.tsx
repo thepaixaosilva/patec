bce491789aeb236faa9dd02987c8caee74de8155
IsabellaOshima <143272475+IsabellaOshima@users.noreply.github.com>
2024-11-16 16:03:42 -0300

    criação tela folha de respostas

--- web/src/app/student/test/page.tsx
+++ web/src/app/student/test/page.tsx
@@ -1,10 +1,28 @@
+import { Stack, Text } from '@chakra-ui/react'
+import { Button } from '@/components/ui/button'
+import Gabarito from '@/components/Gabarito'
+
 export const metadata = {
-  title: 'Seu Título Padrão Aqui',
-  icons: {
-    icon: '/favicon.ico',
-  },
+  title: 'Folha de respostas',
 }
+const disciplinas = ['Matemática', 'Português', 'História']
 
 export default function Test() {
-  return <div className=""></div>
+  return (
+    <div>
+      <div className="flex items-center justify-center m-4">
+        <Stack>
+          <Text textStyle="3xl">Folha de Respostas</Text>
+        </Stack>
+      </div>
+
+      <Gabarito disciplina={disciplinas}></Gabarito>
+
+      <div className="flex items-center justify-center m-4">
+        <Button className=" bg-blue-500 text-white px-4 py-2 rounded w-40 hover:scale-105 hover:bg-blue-600" type="submit">
+          Enviar respostas
+        </Button>
+      </div>
+    </div>
+  )
 }
